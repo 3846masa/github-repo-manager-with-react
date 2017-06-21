dc9c78a25ec37bbae76c567417aaed80f2c6fe6e
3846masa <3846masahiro+git@gmail.com>
2017-06-21 23:17:16 +0900

    Init.

---
+++ fuse.js
@@ -0,0 +1,43 @@
+const {
+  FuseBox,
+  CSSModules,
+  CSSPlugin,
+  TypeScriptHelpers,
+  WebIndexPlugin,
+  UglifyESPlugin,
+} = require('fuse-box');
+const isProduction = process.env['NODE_ENV'] === 'production';
+
+const fuse = FuseBox.init({
+  homeDir: 'src',
+  output: 'dist/scripts/$name.js',
+  tsConfig: 'tsconfig.json',
+  plugins: [
+    TypeScriptHelpers(),
+    [
+      CSSModules({
+        useDefault: false,
+      }),
+      CSSPlugin(),
+    ],
+    WebIndexPlugin({
+      path: './scripts',
+      target: '../index.html',
+      template: 'src/index.html',
+      async: true,
+    }),
+    isProduction && UglifyESPlugin(),
+  ],
+});
+
+const app = fuse.bundle('app').instructions('> index.tsx');
+
+if (!isProduction) {
+  fuse.dev({
+    root: 'dist',
+    port: 8000,
+  });
+  app.watch().hmr();
+}
+
+fuse.run();
